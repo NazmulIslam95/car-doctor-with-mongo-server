f49c3892970e120372ae8440f060c506c575700c
NazmulIslam95 <nazmulislamctg@gmail.com>
2024-01-13 21:48:48 +0600

    jwt configured

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -7,8 +8,13 @@ const app = express();
 const port = process.env.port || 5000
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ww6tl6.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,6 +27,38 @@ const client = new MongoClient(uri, {
     }
 });
 
+// custom middleware
+const logger = async (req, res, next) => {
+    console.log('Called:', req.hostname, req.originalUrl);
+    next()
+}
+
+// const verifyToken = async (req, res, next) => {
+//     const token = req.cookies?.token;
+//     if (!token) {
+//         res.status(401).send({ message: 'Access Denied' })
+//     }
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         if (err) {
+//             console.log(err);
+//             res.status(401).send({ message: 'Access Denied' })
+//         }
+//         console.log('value in the token is', decoded);
+
+//         next()
+//     })
+// }
+
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token in middleware:', token);
+    if (!token) {
+        return res.status(401).send({ message: 'Access Denied' })
+    }
+    next();
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -31,46 +69,66 @@ async function run() {
 
         // auth related api
 
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res.send(token);
-            console.log(token);
-        });
+        app.post('/jwt',
+            logger,
+            async (req, res) => {
+                const user = req.body;
+                console.log(user);
+                const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+
+                res
+                    .cookie('token', token, {
+                        httpOnly: true,
+                        secure: false,
+                        // sameSite: 'none'
+                    })
+                    .send({ success: true });
+                console.log(token);
+            });
 
         // serviceCollection related operations start
-        app.get('/services', async (req, res) => {
-            const result = await serviceCollection.find().toArray()
-            res.send(result)
-        })
+        app.get('/services',
+            logger,
+            verifyToken,
+            async (req, res) => {
+                const result = await serviceCollection.find().toArray()
+                res.send(result)
+            })
 
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
+        app.get('/services/:id',
+            logger,
+            // verifyToken, 
+            async (req, res) => {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) }
 
-            // const options = {
-            //     projection: { title: 1, price: 1, service_id: 1 ,img: 1},
-            // };
+                // const options = {
+                //     projection: { title: 1, price: 1, service_id: 1 ,img: 1},
+                // };
 
-            // const result = await serviceCollection.findOne(query, options)
-            const result = await serviceCollection.findOne(query)
-            res.send(result);
-        })
+                // const result = await serviceCollection.findOne(query, options)
+                const result = await serviceCollection.findOne(query)
+                res.send(result);
+            })
 
         // serviceCollection related operations end
 
         // bookingCollection related operations start
 
-        app.get('/bookings', async (req, res) => {
-            console.log(req.query.email);
-            let query = {};
-            if (req.query?.email) {
-                query = { email: req.query.email }
-            }
-            const result = await bookingCollection.find(query).toArray()
-            res.send(result)
-        })
+        app.get('/bookings',
+            logger,
+            verifyToken,
+            async (req, res) => {
+                console.log(req.query.email);
+                // console.log('This token from client side', req.cookies.token);
+                let query = {};
+                if (req.query?.email) {
+                    query = { email: req.query.email }
+                }
+                const result = await bookingCollection.find(query).toArray()
+                res.send(result)
+            })
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
